70bbf53d660c19b8c72285636a4cb6469f6ce1f9
foxenko <ilyabro65410@gmail.com>
2024-04-21 21:08:31 +0500

    moved data utils

--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const { readData, writeData } = require("./parse");
-const gamesRouter = require("./games");
-const mainRouter = require("./main");
-
-module.exports = { readData, writeData, gamesRouter, mainRouter };
