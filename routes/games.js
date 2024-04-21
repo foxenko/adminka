1145883edada0542c2221b3e08a03e3e65bda250
foxenko <ilyabro65410@gmail.com>
2024-04-21 20:06:25 +0500

    Update games.js

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,6 @@
-const gamesRouter = require("express").Router(); // Создали роутер
-const { readData, writeData } = require("../utils/data"); // Чтение и запись данных в JSON-файл
+const gamesRouter = require("express").Router();
+const { readData, writeData } = require("../utils/data");
 
-// Получим игры из JSON-файла и отправим в ответ на запрос
 const getAllGames = async (req, res) => {
   const games = await readData("./data/games.json");
   if (!games) {
@@ -17,7 +16,6 @@ const getAllGames = async (req, res) => {
 };
 
 const deleteGame = async (req, res) => {
-  // Получим данные из файла
   const games = await readData("./data/games.json");
   if (!games) {
     res.status(400);
@@ -29,22 +27,16 @@ const deleteGame = async (req, res) => {
   }
   req.games = games;
 
-  // Прочитаем запрашиваемый id игры из запроса
   const id = Number(req.params.id);
 
-  // Найдём игру, которую хотят удалить, в общем массиве с играми по id
   req.game = req.games.find((item) => item.id === id);
 
-  // Найдём индекс удаляемой игры в общем массиве игр
   const index = req.games.findIndex((item) => item.id === req.game.id);
 
-  // Удалим из массива игр игру
   req.games.splice(index, 1);
 
-  // Запишем обновлённый массив игр в JSON-файл
   await writeData("./data/games.json", req.games);
 
-  // Вернём ответ о проделанной операции с данными о играх
   res.send({
     games: req.games,
     updated: req.game,
