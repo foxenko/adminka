086f8c9963107197e6477481ac0242657d3d6531
foxenko <ilyabro65410@gmail.com>
2024-04-22 20:20:35 +0500

    Update games.js

--- controllers/games.js
+++ controllers/games.js
@@ -1,5 +1,3 @@
-const { readData, writeData } = require("../utils/data");
-
 const sendAllGames = async (req, res) => {
   res.send(req.games);
 };
@@ -10,13 +8,5 @@ const sendUpdatedGames = async (req, res) => {
     updated: req.updatedObject,
   });
 };
-// updatedObject
 
-const addGame = async (req, res) => {
-  res.send({
-    games: req.games,
-    updated: req.updatedObject,
-  });
-};
-
-module.exports = { sendAllGames, sendUpdatedGames, addGame };
+module.exports = { sendAllGames, sendUpdatedGames };
