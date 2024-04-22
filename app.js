71fe9521c6e6db7628ed8601f55cba29beed805a
foxenko <ilyabro65410@gmail.com>
2024-04-22 19:25:49 +0500

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("./middlewares/cors");
 const { mainRouter, gamesRouter } = require("./routes");
 
 const app = express();
 const PORT = 3000;
 
 app.use(
+  cors,
   express.static(path.join(__dirname, "public")),
   bodyParser.json(),
   mainRouter,
